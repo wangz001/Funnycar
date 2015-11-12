74e82a28cd8db754b8bc63244f68ac6b83a3e58b
wangz001 <bwlang@sina.cn>
2015-11-12 21:34:24 +0800

    tuofengWeb
    delete tfapi

---
+++ TuoFeng/TuoFengWeb/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TuoFengWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+}
+}
---
+++ TuoFeng/TuoFengWeb/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using System.Web.Mvc;
+
+namespace TuoFengWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return null;
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            return null;
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return null;
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return null;
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
